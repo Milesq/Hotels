e4cb8d3b9533a3d290e0144e73e0ed7428227bc4
Milesq <wmilosz88@gmail.com>
2019-04-07 19:17:47 +0200

    Add regulamin

---
+++ pages/reg.vue
@@ -0,0 +1,44 @@
+<template>
+  <main class="text">
+    <h1>Regulamin</h1>
+    <span>Lorem ipsum dolor sit amet consectetur adipisicing
+      elit. Voluptatem maiores itaque provident minus quisquam voluptatum
+      voluptates vero repellat a, voluptate voluptas ut ducimus
+      iure nobis in nam commodi cum sunt?
+    </span>
+
+    <article>
+      <h3 class="header">Postanowienia ogólne</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Voluptatem maiores itaque provident minus quisquam voluptatum
+        voluptates vero repellat a, voluptate voluptas ut ducimus
+        iure nobis in nam commodi cum sunt?
+      </p>
+    </article>
+    <article>
+      <h3 class="header">Wyrażanie opini oraz wprowadzanie danych</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Voluptatem maiores itaque provident minus quisquam voluptatum
+        voluptates vero repellat a, voluptate voluptas ut ducimus
+        iure nobis in nam commodi cum sunt?
+      </p>
+    </article>
+    <article>
+      <h3 class="header">Odpowiedzialność</h3>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Voluptatem maiores itaque provident minus quisquam voluptatum
+        voluptates vero repellat a, voluptate voluptas ut ducimus
+        iure nobis in nam commodi cum sunt?
+      </p>
+    </article>
+  </main>
+</template>
+
+<script>
+export default {
+  layout: 'static'
+};
+</script>
