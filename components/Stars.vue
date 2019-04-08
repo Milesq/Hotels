d32da6c43b8515f6f776e34112a03c6613c2988c
Milesq <wmilosz88@gmail.com>
2019-04-08 16:49:00 +0200

    Add html for filters in search result page

---
+++ components/Stars.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="stars">
+    <template v-for="i in 5">
+      <input @click="send(i)" :key="i" type="radio" :id="'star' + i" name="star">
+      <label :key="i" :for="'star' + i"></label>
+    </template>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['value'],
+  methods: {
+    send(i) {
+      this.$emit('input', 6 - i);
+    }
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.stars {
+  height: 30px;
+  width: 200px;
+  padding: 10px;
+  border-radius: 15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row-reverse;
+
+  input {
+    position: absolute;
+    left: -99999px;
+  }
+}
+
+label {
+  width: 30px;
+  height: 30px;
+  padding: 5px;
+  color: transparent;
+
+  &::before {
+    content: '★';
+    color: #c0c0c0;
+    font-size: 30px;
+    line-height: 30px;
+    margin-right: -100%;
+  }
+}
+
+label:hover::before,
+label:hover ~ label::before,
+.stars input:checked ~ label::before {
+  color: #1976d3;
+}
+</style>
--- pages/search/_city.vue
+++ pages/search/_city.vue
@@ -1,7 +1,46 @@
 <template>
   <section class="container">
-    <nav style="background-color: red">nav</nav>
-    <section style="background-color: blue" class="results">
+    <nav>
+      <label>
+        Rodzaj obiektu
+        <select v-model="category">
+          <option value="aquapark">Aquapark</option>
+          <option value="basen">Basen</option>
+          <option value="sauna">Sauna</option>
+        </select>
+      </label>
+      <span>
+        Atrakcje
+        <label
+          v-for="attraction in possibilityAttractions"
+          :key="attraction[1]"
+          style="display: block">
+            {{ attraction[0] }}
+          <input type="checkbox" :value="attraction[1]" v-model="attractions">
+        </label>
+      </span>
+      <label>
+        Odległość od miasta
+        <input
+          type="range"
+          min="0"
+          max="200"
+          v-model="r">
+        {{ r }}km
+      </label>
+      <label>
+        Minimalna ocena
+        <Stars v-model="rating" />
+      </label>
+      <label>
+        Godziny otwarcia
+        <span>
+          Od: <input type="number" min="0" max="24" v-model="openHours.open">
+          Do: <input type="number" min="0" max="24" v-model="openHours.closed">
+        </span>
+      </label>
+    </nav>
+    <section class="results">
       <h1>Baseny - {{ $route.params.city }}</h1>
       <ObjectInfo />
     </section>
@@ -10,10 +49,30 @@
 
 <script>
 import ObjectInfo from '~/components/ObjectInfo.vue';
+import Stars from '~/components/Stars.vue';
 
 export default {
+  data() {
+    return {
+      category: 'aquapark',
+      attractions: [],
+      r: 100,
+      rating: 0,
+      openHours: {
+        open: 8,
+        closed: 22
+      },
+      possibilityAttractions: [
+        ['Basen sportowy', 'sports-swimming-pool'],
+        ['Basen rekreacyjny', 'recreational-pool'],
+        ['Sauna sucha', 'dry-sauna'],
+        ['Brodzik dla dzieci', 'paddling']
+      ]
+    };
+  },
   components: {
-    ObjectInfo
+    ObjectInfo,
+    Stars
   },
   layout: 'static'
 };
