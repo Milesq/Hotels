37190cd5fdaa88dde4bae0c2d2e2fe6ba205421c
Milesq <wmilosz88@gmail.com>
2019-04-10 22:10:35 +0200

    Add content for post

---
+++ components/Comments.vue
@@ -0,0 +1,15 @@
+<template>
+  <section>
+    komentarze
+  </section>
+</template>
+
+<script>
+export default {
+
+};
+</script>
+
+<style>
+
+</style>
--- pages/blog/_id.vue
+++ pages/blog/_id.vue
@@ -1,18 +1,40 @@
 <template>
-  <section>
-    post
+  <section class="wrapper">
+    <article class="post" v-html="data"></article>
+    <Comments for="blog" />
   </section>
 </template>
 
 <script>
+import Comments from '@/components/Comments.vue';
+
 export default {
   validate({ params }) {
     return /^\d+$/.test(params.id);
   },
-  layout: 'static'
+  asyncData({ params }) {
+    return {
+      data: `
+      post ${params.id} <img src="https://placeimg.com/800/500/any">`
+    };
+  },
+  layout: 'static',
+  components: {
+    Comments
+  }
 };
 </script>
 
-<style>
+<style scoped>
+.wrapper {
+  width: 100%;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 20px 70px;
+  background-color: #fff;
+}
 
+.post {
+  text-align: center;
+}
 </style>
