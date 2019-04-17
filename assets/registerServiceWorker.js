809a98875ede6bc2ad443068f109f74d8a4ad5a9
Milesq <wmilosz88@gmail.com>
2019-04-17 22:15:08 +0200

    Add better service worker

--- assets/registerServiceWorker.js
+++ assets/registerServiceWorker.js
@@ -4,11 +4,6 @@ export default () => {
   if (!window.caches || !navigator.serviceWorker)
     return;
 
-  caches.open('poolsOfflineCache')
-    .then(cache => {
-      cache.add('/offline');
-    });
-
   navigator.serviceWorker.register('/sw.js')
     .then(() => console.log('Service worker registered succesfull!'))
     .catch(err => console.log(`Something was wrong! ${err}`));
