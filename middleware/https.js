6697ca8b2f6cca5b21e94d45e81b14ec78a4ea87
Milesq <wmilosz88@gmail.com>
2019-04-21 22:03:14 +0200

    Disable service worker and https

--- middleware/https.js
+++ middleware/https.js
@@ -1,6 +1,11 @@
-export default ({ req: { headers: { referer: address } }, isDev, redirect }) => {
-  if (!isDev
-    && /^http/.test(address)) {
-    redirect(address.replace('http', 'https'));
-  }
+// export default ({ req: { headers: { referer: address } }, isDev, redirect }) => {
+  // if this line is execute on client side, referer is undefined
+//   if (!isDev
+//     && /^http/.test(address)) {
+//     redirect(address.replace('http', 'https'));
+//   }
+// };
+
+export default () => {
+  console.log('');
 };
