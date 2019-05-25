e7e2b0544e44caee8231075a1b2312db3de79969
Milesq <wmilosz88@gmail.com>
2019-05-25 15:04:10 +0200

    Disable service worker

--- static/sw.js
+++ static/sw.js
@@ -1,37 +1,37 @@
-/* eslint-disable */
+// /* eslint-disable */
 
-const currentCache = {
-  offline: 'offline-cache-site'
-};
+// const currentCache = {
+//   offline: 'offline-cache-site'
+// };
 
-const offlinePage = '/offline';
+// const offlinePage = '/offline';
 
-const assets = [
-  offlinePage
-];
+// const assets = [
+//   offlinePage
+// ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-      caches.open(currentCache.offline).then(cache => {
-          cache.addAll(assets);
-      })
-  );
-});
+// self.addEventListener('install', event => {
+//   event.waitUntil(
+//       caches.open(currentCache.offline).then(cache => {
+//           cache.addAll(assets);
+//       })
+//   );
+// });
 
-self.addEventListener('fetch', event => {
-  if (event.request.mode === 'navigate'
-      || (event.request.method === 'GET'
-      && event.request.headers.get('accept').includes('text/html'))) {
-      event.respondWith(
-          fetch(event.request.url).catch(() => {
-              return caches.match(offlinePage);
-          })
-      );
-  } else {
-      event.respondWith(caches.match(event.request)
-          .then(resp => {
-              return resp || fetch(event.request);
-          })
-      );
-  }
-});
+// self.addEventListener('fetch', event => {
+//   if (event.request.mode === 'navigate'
+//       || (event.request.method === 'GET'
+//       && event.request.headers.get('accept').includes('text/html'))) {
+//       event.respondWith(
+//           fetch(event.request.url).catch(() => {
+//               return caches.match(offlinePage);
+//           })
+//       );
+//   } else {
+//       event.respondWith(caches.match(event.request)
+//           .then(resp => {
+//               return resp || fetch(event.request);
+//           })
+//       );
+//   }
+// });
