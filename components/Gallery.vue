0c6b236cbb2a1374033cdf5ebfb24c01ef43b9aa
Milesq <wmilosz88@gmail.com>
2019-06-16 19:05:20 +0200

    Remove animation

--- components/Gallery.vue
+++ components/Gallery.vue
@@ -1,14 +1,7 @@
 <template>
-    <div class="flip-card gallery">
-      <div class="flip-card-inner" ref="wrapper">
-        <div class="flip-card-front">
-          <div class="gallery__image" ref="flipCardFront"></div>
-        </div>
-        <div class="flip-card-back">
-          <div class="gallery__image" ref="flipCardBack"></div>
-        </div>
-      </div>
-    </div>
+  <div class="gallery">
+    <div class="gallery__image" ref="image"></div>
+  </div>
 </template>
 
 <script>
@@ -25,8 +18,7 @@ export default {
   },
   data() {
     return {
-      id: 0,
-      isFlipped: false
+      id: 0
     };
   },
   methods: {
@@ -37,43 +29,17 @@ export default {
   },
   watch: {
     id(id) {
-      this.$refs[this.isFlipped ? 'flipCardFront' : 'flipCardBack'].style.backgroundImage = `url(${this.images[id]})`;
-      this.$refs.wrapper.style.transform = this.isFlipped ? '' : 'rotateX(-180deg)';
-      this.isFlipped = !this.isFlipped;
+      this.$refs.image.style.backgroundImage = `url(${this.images[id]})`;
     }
   },
   mounted() {
-    this.$refs.flipCardFront.style.backgroundImage = `url(${this.images[0]})`;
+    this.$refs.image.style.backgroundImage = `url(${this.images[0]})`;
     setInterval(this.change, this.interval);
   }
 };
 </script>
 
 <style scoped lang="scss">
-.flip-card {
-  background-color: transparent;
-  perspective: 10000px;
-}
-
-.flip-card-inner {
-  position: relative;
-  width: 100%;
-  height: 100%;
-  text-align: center;
-  transition: transform 1.2s;
-  transform-style: preserve-3d;
-}
-
-.flip-card-front, .flip-card-back {
-  position: absolute;
-  width: 100%;
-  height: 100%;
-  backface-visibility: hidden;
-}
-
-.flip-card-back {
-  transform: rotateX(180deg);
-}
 
 .gallery {
   height: 600px;
